3c3ed4be8064e140ba6afae56479ca74f2b9d34e
orhanerdogan <orhanerdogan@localhost>
2022-10-13 22:21:36 -0500

    10/13/2022 Code -ErdoganCelikKaratas Team

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -1,11 +1,33 @@
 package studio4;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import javax.swing.JFileChooser;
+import javax.swing.SwingUtilities;
+
+import edu.princeton.cs.introcs.StdDraw;
+
 import java.awt.Color;
+import java.util.Scanner;
+
+
 
 import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
 		
+		StdDraw.clear();
+		StdDraw.setPenColor(255, 255, 255);
+		StdDraw.filledRectangle(0, 0, 1, 1);
+		StdDraw.setPenColor(0, 0, 0);
+		StdDraw.filledRectangle(0.5, 0.5, 0.3, 0.25);
+		StdDraw.setPenColor(128, 0, 32);
+		StdDraw.filledCircle(0.5, 0.5, 0.15);
+		StdDraw.setPenColor(255, 255, 0);
+		StdDraw.filledCircle(0.5, 0.5, 0.075);
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -9,6 +9,8 @@ import javax.swing.SwingUtilities;
 
 import edu.princeton.cs.introcs.StdDraw;
 
+import java.awt.Color;
+
 /**
  * @author Dennis Cosgrove (http://www.cse.wustl.edu/~cosgroved/)
  */
@@ -19,6 +21,51 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
+		String ShapeType = in.next();
+		int redComponent = in.nextInt();
+		int greenComponent = in.nextInt();
+		int blueComponent = in.nextInt();
+		boolean isFilled = in.nextBoolean();
+		double parameterOne  = in.nextDouble();
+		double parameterTwo = in.nextDouble();
+		double parameterThree = in.nextDouble();
+		double parameterFour = in.nextDouble();
+
 		
+		
+		StdDraw.setPenColor(new Color (redComponent, greenComponent, blueComponent));
+		if (ShapeType.equals("rectangle")) {
+			System.out.println("Hello");
+			if (isFilled){
+			StdDraw.filledRectangle(parameterOne, parameterTwo, parameterThree, parameterFour);
+			}
+			else {
+			StdDraw.rectangle(parameterOne, parameterTwo, parameterThree, parameterFour);
+			}
+		}
+		if (ShapeType.equals("ellipse")) {
+			if (isFilled){
+			StdDraw.filledEllipse(parameterOne, parameterTwo, parameterThree, parameterFour);
+			}
+			else {
+			StdDraw.ellipse(parameterOne, parameterTwo, parameterThree, parameterFour);
+			}
+		}
+		if (ShapeType.equals("triangle")) {
+			if (isFilled) {
+				double parameterFive = in.nextDouble();
+				double parameterSix = in.nextDouble();
+				double [] x = {parameterOne, parameterThree, parameterFive};
+				double [] y = {parameterTwo, parameterFour, parameterSix};
+				StdDraw.filledPolygon(x, y);
+			} else {
+				double parameterFive = in.nextDouble();
+				double parameterSix = in.nextDouble();
+				double [] x = {parameterOne, parameterThree, parameterFive};
+				double [] y = {parameterTwo, parameterFour, parameterSix};
+				StdDraw.polygon(x, y);
+			}
+		}
+
 	}
 }
